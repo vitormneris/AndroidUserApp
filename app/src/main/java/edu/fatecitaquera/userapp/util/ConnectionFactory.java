4bd9067b0f993e33206537495bf8514c2eb00155
vitormneris <joaomoreiraneris0@gmail.com>
2024-10-30 08:21:31 -0300

    Atualização do IP

--- app/src/main/java/edu/fatecitaquera/userapp/util/ConnectionFactory.java
+++ app/src/main/java/edu/fatecitaquera/userapp/util/ConnectionFactory.java
@@ -1,5 +1,5 @@
 package edu.fatecitaquera.userapp.util;
 
 public class ConnectionFactory  {
-    public static String serverIP = "18.188.197.165";
+    public static String serverIP = "52.15.228.213";
 }
